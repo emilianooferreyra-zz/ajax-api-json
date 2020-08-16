2ca68483e95c6f92c018f13b865735f876fef591
Emiliano Ferreyra <emiliano.ferreyra@mediamonks.com>
2020-08-16 16:31:55 -0300

    change chucknorris api

--- script.ts
+++ script.ts
@@ -32,18 +32,44 @@ const output = document.querySelector('.output');
 //     })
 //   }).catch(error => { console.log(error) });
 
-const url = 'https://en.wikipedia.org/api/rest_v1/feed/featured/2020/01/01';
-fetch(
-  url, {
+// const url = 'https://en.wikipedia.org/api/rest_v1/feed/featured/2020/01/01';
+// fetch(
+//   url, {
+//   "method": "GET"
+// })
+//   .then(response => {
+//     return response.json();
+//   }).then(data => {
+//     console.log(data.onthisday);
+//     data.onthisday.forEach(day => {
+//       console.log(day.text);
+//       output.innerHTML += `${day.text}<br>`;
+//     })
+//   }).catch(error => {
+//     console.log(error);
+//   })
+
+// const url = 'https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22';
+// fetch(url,{
+//   "method" :"GET"
+// }).then(res => {
+//   return res.json()
+// }).then(data => {
+//   console.log(data);
+// }).catch(err => {
+//   console.log(err);
+// })
+
+const url = 'https://api.chucknorris.io/jokes/search?query=big';
+fetch(url, {
   "method": "GET"
-})
-  .then(response => {
-    return response.json();
-  }).then(data => {
-    console.log(data.onthisday);
-    data.onthisday.forEach(day => {
-      console.log(day.text)
-    })
-  }).catch(error => {
-    console.log(error);
+}).then(res => {
+  return res.json()
+}).then(data => {
+  console.log(data.result);
+  data.result.forEach(joke => {
+    console.log(joke.value);
   })
+}).catch(err => {
+  console.log(err);
+})
