82a8a1b1548229516ec5cb006d913167221d04d4
Emiliano Ferreyra <emiliano.ferreyra@mediamonks.com>
2020-08-16 16:07:18 -0300

    change wikipedia api

--- script.ts
+++ script.ts
@@ -15,17 +15,35 @@ const output = document.querySelector('.output');
 //     });
 //   })
 
-const url = 'https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc';
-fetch(url)
-  .then((response) => {
-    return response.json()
-  })
-  .then(data => {
-    console.log(data.items);
-    data.items.forEach(item => {
-      console.log(item.id);
-      output.innerHTML += `
-        ${item.description} 
-        <br>`;
+// const url = 'https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc';
+// fetch(url, {
+//   method: "GET"
+// })
+//   .then((response) => {
+//     return response.json()
+//   })
+//   .then(data => {
+//     console.log(data.items);
+//     data.items.forEach(item => {
+//       console.log(item.id);
+//       output.innerHTML += `
+//         ${item.description} 
+//         <br>`;
+//     })
+//   }).catch(error => { console.log(error) });
+
+const url = 'https://en.wikipedia.org/api/rest_v1/feed/featured/2020/01/01';
+fetch(
+  url, {
+  "method": "GET"
+})
+  .then(response => {
+    return response.json();
+  }).then(data => {
+    console.log(data.onthisday);
+    data.onthisday.forEach(day => {
+      console.log(day.text)
     })
-  });
+  }).catch(error => {
+    console.log(error);
+  })
